69fc780f05f6f788c6d382484d3bd0a1bbd7fab8
李盼 <13243744257@139.com>
2016-08-12 18:30:36 +0800

    ExcelUtils工具类

---
+++ src/main/java/lpp/tools/io/ExcelUtils.java
@@ -0,0 +1,21 @@
+/**
+ * 文件名：ExcelUtils.java
+ * 创建日期： 2016年8月12日
+ * 作者：     lipanpan
+ * Copyright (c) 2009-2011 服务端
+ * All rights reserved.
+ 
+ * 修改记录：
+ * 	1.修改时间：2016年8月12日
+ *   修改人：lipanpan
+ *   修改内容：
+ */
+package lpp.tools.io;
+
+/**
+ * 功能描述：Excel操作工具类
+ */
+public abstract class ExcelUtils
+{
+
+}
--- src/main/java/lpp/tools/io/ZipUtils.java
+++ src/main/java/lpp/tools/io/ZipUtils.java
@@ -1,15 +1,15 @@
 /**
-* 文件名：ZipUtils.java
-* 创建日期： 2016年7月31日
-* 作者：     lipanpan
-* Copyright (c) 2009-2011 无线开发室
-* All rights reserved.
+ * 文件名：ZipUtils.java
+ * 创建日期： 2016年7月31日
+ * 作者：     lipanpan
+ * Copyright (c) 2009-2011 无线开发室
+ * All rights reserved.
  
-* 修改记录：
-* 	1.修改时间：2016年7月31日
-*   修改人：lipanpan
-*   修改内容：
-*/
+ * 修改记录：
+ * 	1.修改时间：2016年7月31日
+ *   修改人：lipanpan
+ *   修改内容：
+ */
 package lpp.tools.io;
 
 import java.io.BufferedInputStream;
@@ -18,6 +18,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -26,17 +28,28 @@ import lpp.tools.comm.StringUtils;
 /**
  * 功能描述：zip解压缩
  */
-public abstract class ZipUtils {
+public abstract class ZipUtils
+{
+
+    /** zip文件标准文件头 */
+    private final static byte[] ZIP_HEADER_FORMAT = { 0x50, 0x4B, 0x03, 0x04 };
 
     /***
      * 压缩文件或文件夹
      * @param filePath
-     * @throws IOException 
+     * @throws IOException
      */
-    public static void zip(String filePath) throws IOException {
-        if (StringUtils.isBlank(filePath)) { return; }
+    public static void zip(String filePath) throws IOException
+    {
+        if (StringUtils.isBlank(filePath))
+        {
+            return;
+        }
         File file = new File(filePath);
-        if (!file.exists()) { return; }
+        if (!file.exists())
+        {
+            return;
+        }
 
         ZipOutputStream out = null;
         BufferedOutputStream bo = null;
@@ -46,7 +59,8 @@ public abstract class ZipUtils {
             if (file.isDirectory())
             {
                 zipFilePath = zipFilePath + file.getName();
-            } else
+            }
+            else
             {
                 String name = file.getName();
                 int suffixIndex = name.lastIndexOf(".");
@@ -60,14 +74,16 @@ public abstract class ZipUtils {
             out = new ZipOutputStream(new FileOutputStream(zipFilePath));
             bo = new BufferedOutputStream(out);
             _zip(out, file, file.getName(), bo);
-        } finally
+        }
+        finally
         {
             StreamUtils.close(bo);
             StreamUtils.close(out);
         }
     }
 
-    private static void _zip(ZipOutputStream out, File f, String base, BufferedOutputStream bo) throws IOException {
+    private static void _zip(ZipOutputStream out, File f, String base, BufferedOutputStream bo) throws IOException
+    {
         if (f.isDirectory())
         {
             File[] fl = f.listFiles();
@@ -79,7 +95,8 @@ public abstract class ZipUtils {
             {
                 _zip(out, fl[i], base + File.separator + fl[i].getName(), bo); // 递归遍历子文件夹
             }
-        } else
+        }
+        else
         {
             out.putNextEntry(new ZipEntry(base)); // 创建zip压缩进入点base
             FileInputStream in = null;
@@ -93,7 +110,8 @@ public abstract class ZipUtils {
                 {
                     bo.write(b);
                 }
-            } finally
+            }
+            finally
             {
                 StreamUtils.close(in);
                 StreamUtils.close(bi);
@@ -105,27 +123,79 @@ public abstract class ZipUtils {
      * 解压zip文件
      * @param filePath
      */
-    public static void unzip(String filePath) throws IOException {
-        if (StringUtils.isBlank(filePath)) { return; }
+    public static void unzip(String filePath) throws IOException
+    {
+        if (StringUtils.isBlank(filePath))
+        {
+            return;
+        }
 
     }
 
     /***
-     * 安全压缩
+     * 加密zip文件
      * @param filePath
      * @throws IOException
      */
-    public static void safeZip(String filePath) throws IOException {
-
+    public static void encryptZipFile(String filePath) throws IOException
+    {
+        // 去除文件头（也可以自定义字节，此处为zip文件标准文件头）
+        File srcFile = new File(filePath);
+        File tempFile = new File(filePath + ".temp");
+        InputStream fis = null;
+        OutputStream fos = null;
+        try
+        {
+            fis = new BufferedInputStream(new FileInputStream(srcFile));
+            fos = new FileOutputStream(tempFile);
+            int len = 0;
+            byte[] b = new byte[1024];
+            fis.read(new byte[ZIP_HEADER_FORMAT.length]);// 去掉文件头加密
+            while ((len = fis.read(b)) > 0)
+            {
+                fos.write(b, 0, len);
+            }
+            srcFile.delete();
+            tempFile.renameTo(srcFile);
+        }
+        finally
+        {
+            StreamUtils.close(fis);
+            StreamUtils.close(fos);
+        }
     }
 
     /***
-     * 安全解压缩
+     * 解密Zip文件
      * @param filePath
      * @throws IOException
      */
-    public static void safeUnzip(String filePath) throws IOException {
-
+    public static void decryptZipFile(String filePath) throws IOException
+    {
+        // 补上文件头（也可以自定义字节，此处为zip文件标准文件头）
+        File srcFile = new File(filePath);
+        File tempFile = new File(filePath + ".temp");
+        InputStream fis = null;
+        OutputStream fos = null;
+        try
+        {
+            fis = new BufferedInputStream(new FileInputStream(srcFile));
+            fos = new FileOutputStream(tempFile);
+            int len = 0;
+            byte[] b = new byte[1024];
+            fos.write(ZIP_HEADER_FORMAT);// 补上文件头
+            while ((len = fis.read(b)) > 0)
+            {
+                fos.write(b, 0, len);
+            }
+            srcFile.delete();
+            tempFile.renameTo(srcFile);
+        }
+        finally
+        {
+            StreamUtils.close(fis);
+            StreamUtils.close(fos);
+        }
     }
 
 }
