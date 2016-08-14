5db63376dc6f7e65dd00d9273f4250e5d2d0ac52
lipanpan <lipanpan@DESKTOP-JD2E61N>
2016-08-14 17:38:11 +0800

    日期处理工具类

---
+++ src/main/java/lpp/tools/comm/DateUtils.java
@@ -0,0 +1,37 @@
+/**
+* 文件名：TimeUtils.java
+* 创建日期： 2016年8月14日
+* 作者：     lipanpan
+* Copyright (c) 2009-2011 无线开发室
+* All rights reserved.
+ 
+* 修改记录：
+* 	1.修改时间：2016年8月14日
+*   修改人：lipanpan
+*   修改内容：
+*/
+package lpp.tools.comm;
+
+import java.util.Date;
+
+/**
+ * 功能描述：日期转换工具类
+ */
+public abstract class DateUtils {
+    
+    static class DateFormat{
+        
+    }
+    
+    /***
+     * 当前日期
+     * @return
+     */
+    public static Date currentDate(){
+         return new Date();
+    }
+    
+    
+    
+
+}
