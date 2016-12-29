e28e722bc77a0db299c19e2b4b027bb6c901231f
李盼 <13243744257@139.com>
2016-12-29 09:36:35 +0800

    代码提交

--- src/main/java/lpp/tools/comm/StringUtils.java
+++ src/main/java/lpp/tools/comm/StringUtils.java
@@ -23,7 +23,9 @@ public abstract class StringUtils {
      * @return
      */
     public static boolean isEmpty(String value) {
-        if (value == null || value.isEmpty()) { return true; }
+        if (value == null || value.isEmpty()) {
+            return true;
+        }
         return false;
     }
 
@@ -33,21 +35,20 @@ public abstract class StringUtils {
      * @return
      */
     public static boolean isNotEmpty(String value) {
-        if (value != null && !value.isEmpty()) { return true; }
+        if (value != null && !value.isEmpty()) {
+            return true;
+        }
         return false;
     }
-    
+
     /**
      * 判断给定的字符串列表是否存在空字符串
      * @param args
      * @return
      */
-    public static boolean isExistEmpty(String... args)
-    {
-        for (String arg : args)
-        {
-            if (isEmpty(arg))
-            {
+    public static boolean isExistEmpty(String... args) {
+        for (String arg : args) {
+            if (isEmpty(arg)) {
                 return true;
             }
         }
@@ -60,7 +61,9 @@ public abstract class StringUtils {
      * @return
      */
     public static boolean isBlank(String value) {
-        if (value == null || value.trim().isEmpty()) { return true; }
+        if (value == null || value.trim().isEmpty()) {
+            return true;
+        }
         return false;
     }
 
@@ -70,25 +73,61 @@ public abstract class StringUtils {
      * @return
      */
     public static boolean isNotBlank(String value) {
-        if (value != null && !value.trim().isEmpty()) { return true; }
+        if (value != null && !value.trim().isEmpty()) {
+            return true;
+        }
         return false;
     }
-    
+
     /**
      * 判断给定的字符串列表是否存在blank字符串
      * @param args
      * @return
      */
-    public static boolean isExistBlank(String... args)
-    {
-        for (String arg : args)
-        {
-            if (isBlank(arg))
-            {
+    public static boolean isExistBlank(String... args) {
+        for (String arg : args) {
+            if (isBlank(arg)) {
                 return true;
             }
         }
         return false;
+    }
+
+    /**
+     * 连接字符集
+     * @param connector
+     * @param args
+     * @return
+     */
+    public static String contactString(String connector, String... args) {
+        StringBuilder sb = new StringBuilder();
+        if (connector == null) {
+            connector = "";
+        }
+        for (String arg : args) {
+            sb.append(arg).append(connector);
+        }
+        String result = sb.toString();
+        if (!connector.equals("")) {
+            return result.substring(0, result.length() - connector.length());
+        }
+        return result;
+    }
+
+    /**
+     * 拆分字符串
+     * @param src
+     * @param regex
+     * @return
+     */
+    public static String[] splitString(String src, String regex) {
+        if (isEmpty(src)) {
+            return null;
+        }
+        if (isEmpty(regex)) {
+            return new String[] {src};
+        }
+        return src.split(regex);
     }
 
 }
